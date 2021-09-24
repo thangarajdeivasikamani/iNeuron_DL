1423c54e1c1ac681e027ec8de3ec7230723a02c9
Thangaraj D <thangarajerode@gmail.com>
2021-09-24 07:16:35 +0530

    Code organized

--- Class 06 September 12/and.py
+++ Class 06 September 12/and.py
@@ -1,23 +1,29 @@
 from utils.model import Perceptron
-from utils.all_utils import prepare_data
+from utils.all_utils import prepare_data,save_plot,save_model
 import pandas as pd
+def main(data,eta,epochs,model_filename,plot_filename):
+ 
+    df = pd.DataFrame(data)
+    print(df)
+    X,y = prepare_data(df)
 
-AND = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,0,0,1],
-}
+    
 
-df = pd.DataFrame(AND)
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X, y)
 
-df
+    _ = model.total_loss()
+    
+    save_model(model, filename=model_filename)
+    save_plot(df, plot_filename, model)
+   
 
-X,y = prepare_data(df)
-
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
-
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
-
-_ = model.total_loss()
+if __name__=='__main__':  ## Entry point
+    AND = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,0,0,1],
+    }
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+    main(data=AND,eta=ETA,epochs=EPOCHS,model_filename="and.model",plot_filename="and.png")
--- Class 06 September 12/utils/all_utils.py
+++ Class 06 September 12/utils/all_utils.py
@@ -5,8 +5,7 @@ import joblib # FOR SAVING MY MODEL AS A BINARY FILE
 from matplotlib.colors import ListedColormap
 import os
 
-
-
+plt.style.use("fivethirtyeight") # THIS IS STYLE OF GRAPHS
 
 def prepare_data(df):
   X = df.drop("y", axis=1)
@@ -22,43 +21,38 @@ def save_model(model, filename):
   joblib.dump(model, filePath)
 
 def save_plot(df, file_name, model):
-  def _create_base_plot(df):
-    df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter")
-    plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
-    plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
-    figure = plt.gcf() # get current figure
-    figure.set_size_inches(10, 8)
+      #Function inside the function-it is local function- we can use internally(kind of protected)
+    def _create_base_plot(df):  # Responsible for printing the data points
+        df.plot(kind="scatter",x="x1",y="x2",c="y",s=100,cmap="winter")  # x1 & x2 point, color,size,color map
+        plt.axhline(y = 0, color = "black",linestyle="--",linewidth =1)   #  Yis zero, X can be anything
+        plt.axvline(x = 0, color = "black",linestyle="--",linewidth =1)   #
+        figure = plt.gcf()  #get current figure
+        figure.set_size_inches(10,8)
+    def _plot_decision_regions(X, y, classfier, resolution=0.02):                 # Plot the decision boundary
+        colors = ("red","blue","lightgreen","grey","cyan")
+        cmap = ListedColormap(colors[:len(np.unique(y))])
+        X = X.values # as  a array
+        x1 = X[:,0] # Separate the values,all rows zero column
+        x2 = X[:,1]
+        x1_min,x1_max = x1.min() -1, x1.max() + 1 # Find the maximum , For increase the visible we will add +1, -1
+        x2_min,x2_max = x2.min() -1, x2.max() + 1
+        # Take the min & max and find the each point co-ordinate
+        xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), 
+                  np.arange(x2_min, x2_max, resolution))
+        print(xx1)
+        print(xx1.ravel()) # Make it as single array.
+        Z = classfier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+        Z = Z.reshape(xx1.shape)
+        plt.contourf(xx1, xx2, Z, alpha=0.2, cmap=cmap) # Find the value xx1 & color map based on Z value
+        plt.xlim(xx1.min(), xx1.max())
+        plt.ylim(xx2.min(), xx2.max())
+        plt.plot()
 
-def _plot_decision_regions(X, y, classfier, resolution=0.02):
-    colors = ("red", "blue", "lightgreen", "gray", "cyan")
-    cmap = ListedColormap(colors[: len(np.unique(y))])
-
-    X = X.values # as a array
-    x1 = X[:, 0] 
-    x2 = X[:, 1]
-    x1_min, x1_max = x1.min() -1 , x1.max() + 1
-    x2_min, x2_max = x2.min() -1 , x2.max() + 1  
-
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), 
-                           np.arange(x2_min, x2_max, resolution))
-    print(xx1)
-    print(xx1.ravel())
-    Z = classfier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.2, cmap=cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-
-    plt.plot()
-
-
-
-  X, y = prepare_data(df)
-
-  _create_base_plot(df)
-  _plot_decision_regions(X, y, model)
-
-  plot_dir = "plots"
-  os.makedirs(plot_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
-  plotPath = os.path.join(plot_dir, file_name) # model/filename
-  plt.savefig(plotPath)
+    X,y = prepare_data(df)
+    _create_base_plot(df)
+    _plot_decision_regions(X, y, model)
+    plot_dir = "plots" # Dir name
+    os.makedirs(plot_dir,exist_ok=True)#ONLY Create the model direct not avaliable
+    plotPath= os.path.join(plot_dir,file_name)# will join model+ file name , Based on operating system it will automatically will join
+    joblib.dump(model,plotPath)
+    plt.savefig(plotPath)
